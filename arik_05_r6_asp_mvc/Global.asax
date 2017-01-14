88afb852b6112175e2ad2a2802cb9d858f87b154
Arnetta Rahma <arnettarahma@gmail.com>
2017-01-14 20:08:01 +0700

    Add project files.

---
+++ arik_05_r6_asp_mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="arik_05_r6_asp_mvc.MvcApplication" Language="C#" %>
---
+++ arik_05_r6_asp_mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(arik_05_r6_asp_mvc.Startup))]
+namespace arik_05_r6_asp_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
